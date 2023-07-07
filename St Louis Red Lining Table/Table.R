a46a0ea646307b97d69fa75ab79108791af44335
Bradford M Dykes <dykesb@gvsu.edu>
2023-07-07 15:56:58 -0400

    All that remains are the pentagon labels in the first column!

--- St Louis Red Lining Table/Table.R
+++ St Louis Red Lining Table/Table.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(gt)
 library(gtExtras)
+# This would be flexible and I have a license for it: https://github.com/CaRdiffR/nounprojectR
 library(fontawesome)
 
 # arrows
@@ -22,75 +23,12 @@ stl_table <- tibble(
 ) 
 
 # descriptive names
-measures <- c("Total\nPopulation", "Median\nHome Value", "Median Household\nIncome", "Owner Occupied\nHousing Units", "Bachelors Degree or\nHigher")
+measures <- c("Total<br>Population", "Median<br>Home Value", "Median Household<br>Income", "Owner Occupied<br>Housing Units", "Bachelors Degree or<br>Higher")
 
 row_types <- c('num', 'dollar', 'dollar', 'pct', 'pct')
 
-# rotated dataframe
-stl_table_rotated <- stl_table %>% 
-  pivot_longer(
-    cols = tot_population:bach_or_higher,
-    names_to = "measure",
-    values_to = "values"
-  ) %>% 
-  pivot_wider(
-    id_cols = measure,
-    names_from = grade,
-    values_from = values
-  ) %>% 
-  mutate(
-    measure = measures,
-    A_diff_B = B - A,
-    A_diff_C = C - A,
-    A_diff_D = D - A,
-    type = row_types,
-    across(where(is.factor), as.character))
 
-stl_table_format <- stl_table_rotated %>% 
-  pivot_longer(cols = A:A_diff_D, names_to = "var", values_to = "values") %>% 
-  pivot_wider(names_from = type, values_from = values) %>% 
-  mutate(num = scales::comma(num, accuracy = 1),
-         dollar = scales::dollar(dollar, accuracy = 1),
-         pct = scales::percent(pct, accuracy = 1)) %>% 
-  pivot_longer(cols = num:pct, names_to = "type", values_to = "values") %>% 
-  select(-type) %>% 
-  na.omit() %>% 
-  pivot_wider(names_from = var, values_from = values) #%>% 
-  # mutate(
-  #   A_diff_B = if_else(str_starts(A_diff_B, "-"),
-  #                      gluing(A_diff_B, NULL,  "down"),
-  #                      gluing(A_diff_B, NULL, "up")),
-  # 
-  #   A_diff_C = if_else(str_starts(A_diff_C, "-"),
-  #                      gluing(A_diff_C, NULL, "down"),
-  #                      gluing(A_diff_C, NULL, "up")),
-  # 
-  #   A_diff_D = if_else(str_starts(A_diff_D, "-"),
-  #                      gluing(A_diff_D, NULL, "down"),
-  #                      gluing(A_diff_D, NULL, "up")),
-    # B = gluing(B, A_diff_B, "together"),
-    # C = gluing(C, A_diff_C, "together"),
-    # D = gluing(D, A_diff_D, "together"),
-    #)
-
-
-stl_table_glued <- rbind(stl_table_format[1,], 
-      c(measures[1], " ", unname(stl_table_format[1, 6]), unname(stl_table_format[1, 7]), unname(stl_table_format[1, 8]), " ", " ", " "), 
-      stl_table_format[2,],
-      c(measures[2], " ", unname(stl_table_format[2, 6]), unname(stl_table_format[2, 7]), unname(stl_table_format[2, 8]), " ", " ", " "),
-      stl_table_format[3,],
-      c(measures[3], " ", unname(stl_table_format[3, 6]), unname(stl_table_format[3, 7]), unname(stl_table_format[3, 8]), " ", " ", " "),
-      stl_table_format[4,],
-      c(measures[4], " ", unname(stl_table_format[4, 6]), unname(stl_table_format[4, 7]), unname(stl_table_format[4, 8]), " ", " ", " "),
-      stl_table_format[5,],
-      c(measures[5], " ", unname(stl_table_format[5, 6]), unname(stl_table_format[5, 7]), unname(stl_table_format[5, 8]), " ", " ", " ")) %>% 
-  select(measure:D) %>% 
-  add_column(names = c(rep("", 10)), .before = "A")
-
-
-
-colors <- c("#81A26B", "#86AEBA", "#CBC361", "#D77085")
-
+# Ian's "add arrow to diffs" function
 gluing <- function(x) {
   lapply(x, function(x) {
     if (grepl("^-", x)) {
@@ -108,13 +46,120 @@ gluing <- function(x) {
       )
     }
   }
-    )
+  )
 }
 
 # Looks pretty good. I still want to move the black text (with arrows) higher up closer to the 
 # corresponding row above
 
+# rotated dataframe
+stl_table_rotated <- stl_table %>% 
+  pivot_longer(
+    cols = tot_population:bach_or_higher,
+    names_to = "measure",
+    values_to = "values"
+  ) %>% 
+  pivot_wider(
+    id_cols = measure,
+    names_from = grade,
+    values_from = values
+  ) %>% 
+  mutate(
+    measure = measures,
+    A_diff_A = NA_integer_,
+    B_diff_A = B - A,
+    C_diff_A = C - A,
+    D_diff_A = D - A,
+    type = row_types)
+  
+stl_table_format <- stl_table_rotated %>% 
+  # https://stackoverflow.com/questions/29873293/dplyr-order-columns-alphabetically-in-r
+  select(measure, order(colnames(.)), type) %>% 
+  mutate(across(A:D_diff_A, 
+                ~case_when(
+                  type == "num" ~ scales::comma(., accuracy = 1), 
+                  type == "dollar"~ scales::dollar(., accuracy = 1),
+                  type == "pct" ~ scales::percent(., accuracy = 1)
+                  )
+                ),
+         across(contains("diff"), gluing)) %>% 
+  select(-type)
+
+colors <- c("#81A26B", "#86AEBA", "#CBC361", "#D77085")
+
+
 # Create a gt table based on preprocessed
+stl_table_format %>% 
+  gt(rowname_col = "measure") %>%
+  tab_stubhead(label = md("<span style='font-size:200%'>St Louis</span><br>
+                          <span style='font-size:100%'>Missouri</span>")) %>%
+  cols_align(align = "left", columns = 1) %>% 
+  # cols_align(align = "center", columns = 2:8) %>% 
+  cols_width(measure ~ px(200), everything() ~ px(175)) %>%
+  gt_highlight_cols(A, 
+                    fill =c("#81A26B")) %>%
+  gt_highlight_cols(B, 
+                    fill = c("#86AEBA")) %>%
+  gt_highlight_cols(C, 
+                    fill = c("#CBC361")) %>%
+  gt_highlight_cols(D, 
+                    fill = c("#D77085")) %>% 
+    tab_footnote(footnote = md("<div style='width:200px; float:left; text-align:right; vertical-align:top;'>&nbsp;</div>
+    <div style='width:50%; float:left;'>This infographics provides a set of Esri demographic indicators based on a snapshot for July 1, 2020.<br>
+  Adjustments have been made to current year models to reflect the impact of the COVID-19 pandemic.<br></div>
+                                <div style='width:15%; float:right; text-align:right; vertical-align:top;'>&copy; 2020 Esri<br></div>")) %>%
+  tab_style(
+    style = list(
+      cell_borders(sides = c("left", "right"),
+                   color = "#F4F6F6",
+                   weight = px(4)),
+      cell_text(size = "large")
+    ),
+    locations = cells_body()
+  ) %>%
+  cols_merge(
+    columns = starts_with("A"),
+    pattern = "<div style='text-align:center; color:#ffffff; font-size:150%'>{1}</div>
+                <div style='color:#81A26B; font-size:75%; width: 100%; margin: 0 auto; text-indent: 50%;'>{2}</div>"
+  ) %>% 
+  cols_merge(
+    columns = starts_with("B"),
+    pattern = "<div style='text-align:center; color:#ffffff; font-size:150%'>{1}</div>
+                 <div style='width:50%; float:left;'></div><div style='width:50%; float:right; color:#000000; font-size:75%; text-align:left;'>{2}</div>"
+  ) %>% 
+  cols_merge(
+    columns = starts_with("C"),
+    pattern = "<div style='text-align:center; color:#ffffff; font-size:150%'>{1}</div>
+                 <div style='width:50%; float:left;'></div><div style='width:50%; float:right; color:#000000; font-size:75%; text-align:left;'>{2}</div>"
+  ) %>% 
+  cols_merge(
+    columns = starts_with("D"),
+    pattern = "<div style='text-align:center; color:#ffffff; font-size:150%'>{1}</div>
+                 <div style='width:50%; float:left;'></div><div style='width:50%; float:right; color:#000000; font-size:75%; text-align:left;'>{2}</div>"
+  ) %>%
+  cols_label(
+    A = md("<div style='text-align:center; font-size:200%;'>A</div>"),
+    B = md("<div style='text-align:center; font-size:200%;'>B</div>"),
+    C = md("<div style='text-align:center; font-size:200%;'>C</div>"),
+    D = md("<div style='text-align:center; font-size:200%;'>D</div>")
+  
+  tab_options(
+    table.background.color = "#F4F6F6",
+    table.border.top.style = "hidden",
+    table.border.bottom.style = "hidden",
+    table_body.border.bottom.style = "hidden",
+    table_body.hlines.style = "hidden",
+    column_labels.font.weight = "bold",
+    footnotes.font.size = "55%",
+    footnotes.border.bottom.style = "hidden",
+    # footnotes.padding.horizontal = px(205), # Find more efficient way to handle - px(#) isn't what I want either
+    column_labels.border.bottom.color = "#F4F6F6",
+    row_group.padding = px(0),
+    row_group.as_column = TRUE,
+    #   table.width = px(1000)
+  )
+
+
 table <- stl_table_glued %>%
   gt(rowname_col = "names", groupname_col = "measure") %>%
   tab_stubhead(label = md("<span style='font-size:100%'>St Louis</span><br><span style='font-size:50%'>Missouri</span>")) %>%
@@ -175,11 +220,15 @@ table <- stl_table_glued %>%
     table_body.hlines.style = "hidden",
     column_labels.font.size = "200%",
     column_labels.font.weight = "bold",
+    footnotes.font.size = 10,
     footnotes.border.bottom.style = "hidden",
+    footnotes.padding.horizontal = 163, # Find more efficient way to handle - px(#) isn't what I want either
     column_labels.border.bottom.color = "#F4F6F6",
     row_group.padding = px(0),
     row_group.as_column = TRUE,
   #   table.width = px(1000)
   )
+
+table
 
 gtsave_extra(table, "St Louis Red Lining Table/table.png")
